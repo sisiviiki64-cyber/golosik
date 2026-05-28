92e3dc494f3aaef20b0b8b3386ab45ff42f0546d
sisiviiki64-cyber <sisiviiki64@gmail.com>
2026-05-28 16:35:18 +0500

    Add files via upload

---
+++ EEQ.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/title%20screen.jpg",
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/4эйф.png",
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/5эйф.png",
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/1эйф.png",
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/2эйф.png",
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/3эйф.png"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "E",
+
+  "R",
+
+  "D",
+
+  "M",
+
+  "K",
+
+  "N"
+
+];
