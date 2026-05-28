1bbb3574989b3dcdbd8a4263232060c37f1eb2f5
sisiviiki64-cyber <sisiviiki64@gmail.com>
2026-05-28 16:48:01 +0500

    Add files via upload

---
+++ AAA.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/title%20screen.jpg",
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/rue.jpg",
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/SHE%20LOOKS%20SO%20GOOD.jpg",
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/Alexa%20Demie.jpg",
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/Cassie%20Howard%20Aesthetic%20🌹Euphoria.jpg",
+
+  "https://raw.githubusercontent.com/sisiviiki64-cyber/golosik/refs/heads/main/It's%20Time%20to%20Talk%20About%20How%20Problematic%20Nate's%20Fixation%20on%20Cassie%20Is%20on%20Euphoria.jpg"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "EIPHORIA",
+
+  "RU",
+
+  "DZYLS",
+
+  "MEDDY",
+
+  "KESSY",
+
+  "NATE"
+
+];
